c7a6697aedeb2219f1142a0a0f6b831cb7ec8444
Ankur Singh <abishek.singh.chauhan@gmail.com>
2022-11-17 17:02:25 +0545

    Initial commit: initialized astro project

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import alpinejs from '@astrojs/alpinejs';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [alpinejs(), tailwind()],
+});
